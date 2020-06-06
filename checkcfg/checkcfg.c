c811a12f46311ff35e1357cb6bc7bef79c883f89
ms <ms-develop@stoffnet.at>
2020-06-06 09:00:48 +0200

    Fix: Copyright notice

--- checkcfg/checkcfg.c
+++ checkcfg/checkcfg.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Marin Atanasov Nikolov <dnaeon@gmail.com>
+ * Copyright (c) 2020 Markus Stoff
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
